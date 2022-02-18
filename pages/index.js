228b286867f71f780982612ff010020580b690a4
jjlstruggle <2020213994@stu.cqupt.edu.cn>
2022-02-18 20:05:04 +0800

    jingtai

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,13 @@ import p4 from "../assets/img/frame4/4.png";
 import xingyun from "../assets/img/frame4/xingyun.png";
 import xingyun2 from "../assets/img/frame4/xingyun2.png";
 import vector3 from "../assets/img/frame5/Vector 43 (1).png";
+import square from "../assets/img/frame6/square.png";
+import chanpin from "../assets/img/frame6/chanpin.png";
+import shijue from "../assets/img/frame6/shijue.png";
+import yidong from "../assets/img/frame6/yidongg.png";
+import yunwei from "../assets/img/frame6/yunwei.png";
+import web from "../assets/img/frame6/web.png";
+import vector4 from "../assets/img/frame7/Vector 43.png";
 
 /*
  *
@@ -41,8 +48,6 @@ import vector3 from "../assets/img/frame5/Vector 43 (1).png";
 export default function Home() {
   const [vh, setVh] = useState(0);
   const [choose, setChoose] = useState(0);
-
-  useEffect(() => {}, []);
 
   useEffect(() => {
     const body = document.querySelector("body");
@@ -744,6 +749,165 @@ export default function Home() {
           }}
         >
           <Image src={vector3}></Image>
+        </div>
+      </div>
+      <div className={styles.frame6} style={{ paddingTop: 118 * vh }}>
+        <div
+          className={styles.square}
+          style={{
+            height: 205 * vh,
+          }}
+        >
+          <div
+            className={styles.pic1}
+            style={{ marginTop: 28 * vh, marginBottom: 13 * vh }}
+          >
+            <Image src={chanpin}></Image>
+          </div>
+          <div
+            className={styles.title2 + " font2"}
+            style={{ marginBottom: 4 * vh }}
+          >
+            产品策划及运营部
+          </div>
+          <div className={styles.title2 + " font1"}>
+            “创意聚集港，产品造梦厂”
+          </div>
+        </div>
+        <div
+          className={styles.square}
+          style={{
+            height: 205 * vh,
+          }}
+        >
+          <div
+            className={styles.pic2}
+            style={{ marginTop: 20 * vh, marginBottom: 4 * vh }}
+          >
+            <Image src={shijue}></Image>
+          </div>
+          <div
+            className={styles.title3 + " font2"}
+            style={{ marginBottom: 4 * vh }}
+          >
+            视觉设计部
+          </div>
+          <div className={styles.title3 + " font1"}>
+            “色彩涂抹梦想，画笔创造未来”
+          </div>
+        </div>
+        <div className={styles.fragment1} style={{ height: 205 * vh }}></div>
+        <div
+          className={styles.square}
+          style={{
+            height: 205 * vh,
+          }}
+        >
+          <div
+            className={styles.pic1}
+            style={{ marginTop: 28 * vh, marginBottom: 16 * vh }}
+          >
+            <Image src={yidong}></Image>
+          </div>
+          <div
+            className={styles.title3 + " font2"}
+            style={{ marginBottom: 4 * vh }}
+          >
+            移动开发部
+          </div>
+          <div className={styles.title3 + " font1"}>“方寸间世界的创造者“</div>
+        </div>
+        <div className={styles.fragment2} style={{ height: 205 * vh }}></div>
+        <div className={styles.square2}>
+          <Image src={square}></Image>
+        </div>
+        <div
+          className={styles.square}
+          style={{
+            height: 205 * vh,
+          }}
+        >
+          <div
+            className={styles.pic2}
+            style={{ marginTop: 9 * vh, marginBottom: 8 * vh }}
+          >
+            <Image src={web}></Image>
+          </div>
+          <div
+            className={styles.title3 + " font2"}
+            style={{ marginBottom: 4 * vh }}
+          >
+            WEB研发部
+          </div>
+          <div className={styles.title3 + " font1"}>
+            “WEB，实现互联网的不可思议”
+          </div>
+        </div>
+        <div
+          className={styles.square}
+          style={{
+            height: 205 * vh,
+          }}
+        >
+          <div
+            className={styles.pic1}
+            style={{ marginTop: 28 * vh, marginBottom: 13 * vh }}
+          >
+            <Image src={yunwei}></Image>
+          </div>
+          <div
+            className={styles.title3 + " font2"}
+            style={{ marginBottom: 4 * vh }}
+          >
+            运维安全部
+          </div>
+          <div className={styles.title3 + " font1"}>
+            “运维安全在，网校没意外”
+          </div>
+        </div>
+      </div>
+      <div className={styles.frame7} style={{ height: 500 * vh }}>
+        <div
+          className={styles.box3}
+          style={{
+            marginTop: 117 * vh,
+          }}
+        >
+          <Image src={vector4}></Image>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-start",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            className={"font2"}
+            style={{
+              height: 34 * vh,
+              marginBottom: 31 * vh,
+              marginTop: 135 * vh,
+            }}
+          >
+            看看
+          </div>
+          <div
+            className={"font3"}
+            style={{
+              height: 50 * vh,
+            }}
+          >
+            我们的21年!
+          </div>
+        </div>
+        <div
+          className={styles.box4}
+          style={{
+            marginTop: 129 * vh,
+          }}
+        >
+          <Image src={square}></Image>
         </div>
       </div>
     </div>
